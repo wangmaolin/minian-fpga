8de154f70487d40f05f96b7a89e06da3dbb84796
phildong <35715936+phildong@users.noreply.github.com>
2024-01-31 18:00:35 -0500

    add minimal example for temporal update

--- simulate_temporal_update.py
+++ simulate_temporal_update.py
@@ -1,8 +1,13 @@
 # %% imports and definition
 import os
 
-from routine.cnmf import compute_trace, update_temporal_cvxpy
+import numpy as np
+import xarray as xr
+
+from routine.cnmf import compute_trace, update_temporal_block
+from routine.minian_functions import open_minian
 from routine.simulation import generate_data
+from routine.utilities import rechunk_like
 
 INT_PATH = "./intermediate/temporal_simulation"
 FIG_PATH = "./figs/temporal_simulation"
@@ -21,12 +26,42 @@ Y, A, C, S, shifts = generate_data(
     tmp_pfire=0.01,
     tmp_tau_d=6,
     tmp_tau_r=1,
-    bg_nsrc=100,
-    bg_tmp_var=2,
-    bg_cons_fac=0.1,
-    bg_smth_var=60,
+    bg_nsrc=0,
+    bg_tmp_var=0,
+    bg_cons_fac=0,
+    bg_smth_var=0,
     mo_stp_var=0,
     mo_cons_fac=0,
     post_offset=1,
     post_gain=50,
+    save_Y=True,
+)
+
+# %% temporal update
+minian_ds = open_minian(os.path.join(INT_PATH, "simulated"))
+Y, A, C = minian_ds["Y"], minian_ds["A"], minian_ds["C"]
+b = rechunk_like(
+    xr.DataArray(
+        np.zeros((A.sizes["height"], A.sizes["width"])),
+        dims=["height", "width"],
+        coords={d: A.coords[d] for d in ["height", "width"]},
+    ),
+    A,
+)
+f = rechunk_like(
+    xr.DataArray(
+        np.zeros((Y.sizes["frame"])),
+        dims=["frame"],
+        coords={"frame": Y.coords["frame"]},
+    ),
+    C,
+)
+YrA = compute_trace(Y, A, b, C, f).compute()
+c, s, b, c0, g = update_temporal_block(
+    np.array(YrA),
+    noise_freq=0.1,
+    p=2,
+    sparse_penal=0.1,
+    max_iters=1000,
+    zero_thres=1e-9,
 )
