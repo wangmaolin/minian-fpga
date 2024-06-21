b249716850dbc03668dbb632a38b1126fe069799
phildong <35715936+phildong@users.noreply.github.com>
2024-06-21 11:55:22 -0400

    implemented upsample (no interpolation) methods

--- simulate_temporal_update.py
+++ simulate_temporal_update.py
@@ -10,36 +10,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import scipy.sparse as sps
 import seaborn as sns
 import xarray as xr
-from scipy.sparse import dia_matrix
 from scipy.spatial.distance import cdist
 from tqdm.auto import tqdm
 
 from routine.cnmf import compute_trace, update_temporal_block
 from routine.minian_functions import open_minian
-from routine.simulation import generate_data
+from routine.simulation import generate_data, tau2AR
 from routine.utilities import norm, rechunk_like
 
 INT_PATH = "./intermediate/temporal_simulation"
 FIG_PATH = "./figs/temporal_simulation"
+PARAM_TAU_D = 6
+PARAM_TAU_R = 1
+PARAM_UPSAMP = 10
 
 os.makedirs(INT_PATH, exist_ok=True)
 os.makedirs(FIG_PATH, exist_ok=True)
+
+
+def thresS(S, nthres, rename=True):
+    Smax = S.max()
+    if rename:
+        return [
+            (S > Smax * th).rename(S.name + "-th_{:.1f}".format(th))
+            for th in np.linspace(0.1, 0.9, nthres)
+        ]
+    else:
+        return [(S > Smax * th) for th in np.linspace(0.1, 0.9, nthres)]
+
 
 # %% generate data
 Y, A, C, S, shifts, C_gt, S_gt = generate_data(
     dpath=INT_PATH,
     ncell=100,
-    upsample=10,
+    upsample=PARAM_UPSAMP,
     dims={"height": 256, "width": 256, "frame": 2000},
     sig_scale=1,
     sz_mean=3,
     sz_sigma=0.6,
     sz_min=0.1,
     tmp_pfire=0.003,
-    tmp_tau_d=6,
-    tmp_tau_r=1,
+    tmp_tau_d=PARAM_TAU_D,
+    tmp_tau_r=PARAM_TAU_R,
     bg_nsrc=0,
     bg_tmp_var=0,
     bg_cons_fac=0,
@@ -84,13 +99,10 @@ f = rechunk_like(
     C_gt,
 )
 YrA = compute_trace(Y, A, b, C_gt, f).compute()
-YrA_interp = YrA.interp(
-    {"frame": C_gt_true.coords["frame"]}, kwargs={"fill_value": "extrapolate"}
-)
-updt_ds = [YrA.rename("YrA"), YrA_interp.rename("YrA_interp")]
-for up_type, cur_YrA in {"org": YrA, "upsamp": YrA_interp}.items():
-    cur_YrA_ps, gs, tns = update_temporal_block(
-        np.array(cur_YrA),
+updt_ds = [YrA.rename("YrA")]
+for up_type, up_factor in {"org": 1, "upsamp": PARAM_UPSAMP}.items():
+    _, _, tns = update_temporal_block(
+        np.array(YrA),
         noise_freq=0.1,
         p=2,
         add_lag=100,
@@ -99,30 +111,39 @@ for up_type, cur_YrA in {"org": YrA, "upsamp": YrA_interp}.items():
         zero_thres=1e-9,
         return_param=True,
     )
+    gs = np.tile(
+        tau2AR(PARAM_TAU_D * up_factor, PARAM_TAU_R * up_factor),
+        (A.sizes["unit_id"], 1),
+    )
     sps_penal = 10
     max_iters = 50
     res = {"C": [], "S": [], "b": [], "C-bin": [], "S-bin": [], "b-bin": [], "scal": []}
-    for y, g, tn in tqdm(
-        zip(np.array(cur_YrA), gs, tns), total=np.array(cur_YrA).shape[0]
-    ):
+    for y, g, tn in tqdm(zip(np.array(YrA), gs, tns), total=np.array(YrA).shape[0]):
         # parameters
-        T = len(y)
-        G = dia_matrix(
+        Torg = len(y)
+        T = Torg * up_factor
+        G = sps.dia_matrix(
             (
                 np.tile(np.concatenate(([1], -g)), (T, 1)).T,
                 -np.arange(len(g) + 1),
             ),
             shape=(T, T),
-        ).todense()
+        ).tocsc()
+        G_inv = sps.linalg.inv(G)
+        rs_vec = np.zeros(T)
+        rs_vec[:up_factor] = 1
+        Rs = sps.coo_matrix(
+            np.stack([np.roll(rs_vec, up_factor * i) for i in range(Torg)], axis=0)
+        )
+        RG = (Rs @ G_inv).todense()
         y = y - y.min()
         y = y / y.max()
         y = y.reshape((-1, 1))
-        G_inv = np.linalg.inv(G)
         # org prob
         c = cp.Variable((T, 1))
         s = cp.Variable((T, 1))
         b = cp.Variable()
-        obj = cp.Minimize(cp.norm(y - c - b) + sps_penal * tn * cp.norm(s))
+        obj = cp.Minimize(cp.norm(y - Rs @ c - b) + sps_penal * tn * cp.norm(s))
         cons = [s == G @ c, c >= 0, s >= 0, b >= 0]
         prob = cp.Problem(obj, cons)
         prob.solve()
@@ -133,7 +154,7 @@ for up_type, cur_YrA in {"org": YrA, "upsamp": YrA_interp}.items():
         c_init = cp.Variable((T, 1))
         s_init = cp.Variable((T, 1))
         b_init = cp.Variable()
-        obj_init = cp.Minimize(cp.norm(y - c_init - b_init))
+        obj_init = cp.Minimize(cp.norm(y - Rs @ c_init - b_init))
         cons_init = [s_init == G @ c_init, c_init >= 0, s_init >= 0, b_init >= 0]
         prob_init = cp.Problem(obj_init, cons_init)
         prob_init.solve()
@@ -141,77 +162,80 @@ for up_type, cur_YrA in {"org": YrA, "upsamp": YrA_interp}.items():
         scale = np.ptp(s_init.value)
         niter = 0
         tol = 1e-6
-        scale_df = []
-        s_bin_df = []
-        opt_s_df = []
-        obj_df = []
-        lb_df = []
+        s_bin_df = pd.DataFrame(
+            {"s_bin": s.value.squeeze(), "frame": np.arange(T), "iter": -1}
+        )
+        scale_df = None
+        opt_s_df = None
+        obj_df = None
+        lb_df = None
         while niter < max_iters:
             c_bin = cp.Variable((T, 1))
             s_bin = cp.Variable((T, 1))
             b_bin = cp.Variable()
             obj = cp.Minimize(
-                cp.norm(y - scale * c_bin - b_bin)
+                cp.norm(y - scale * Rs @ c_bin - b_bin)
                 # + sps_penal * tn * cp.norm(s_bin)
             )
             cons = [s_bin == G @ c_bin, c_bin >= 0, b_bin >= 0, s_bin >= 0, s_bin <= 1]
             prob = cp.Problem(obj, cons)
             prob.solve()
-            svals = []
             objvals = []
-            for thres in np.linspace(s_bin.value.min(), s_bin.value.max(), 1000):
-                s_thres = s_bin.value >= thres
-                svals.append(s_thres)
-                objvals.append(
-                    np.linalg.norm(y / scale - G_inv @ s_thres - b_bin.value / scale)
-                    * scale
-                    # + sps_penal * tn * np.linalg.norm(s_thres, 1)
-                )
+            svals = thresS(s_bin.value, 1000, rename=False)
+            objvals = [
+                np.linalg.norm(y - scale * (RG @ ss) - b_bin.value) for ss in svals
+            ]
             opt_idx = np.argmin(objvals)
             opt_s = svals[opt_idx]
-            scale_df.append(pd.DataFrame([{"scale": scale, "iter": niter}]))
-            s_bin_df.append(
-                pd.DataFrame(
-                    {
-                        "s_bin": s_bin.value.squeeze(),
-                        "frame": np.arange(T),
-                        "iter": niter,
-                    }
-                )
+            opt_obj = objvals[opt_idx]
+            try:
+                opt_obj_last = obj_df["obj"].min()
+            except TypeError:
+                opt_obj_last = np.inf
+            scale_df = pd.concat(
+                [scale_df, pd.DataFrame([{"scale": scale, "iter": niter}])]
             )
-            opt_s_df.append(
-                pd.DataFrame(
-                    {"opt_s": opt_s.squeeze(), "frame": np.arange(T), "iter": niter}
-                )
+            s_bin_df = pd.concat(
+                [
+                    s_bin_df,
+                    pd.DataFrame(
+                        {
+                            "s_bin": s_bin.value.squeeze(),
+                            "frame": np.arange(T),
+                            "iter": niter,
+                        }
+                    ),
+                ]
             )
-            obj_df.append(pd.DataFrame([{"obj": objvals[opt_idx], "iter": niter}]))
-            lb_df.append(pd.DataFrame([{"lb": prob.value, "iter": niter}]))
+            opt_s_df = pd.concat(
+                [
+                    opt_s_df,
+                    pd.DataFrame(
+                        {"opt_s": opt_s.squeeze(), "frame": np.arange(T), "iter": niter}
+                    ),
+                ]
+            )
+            obj_df = pd.concat(
+                [obj_df, pd.DataFrame([{"obj": opt_obj, "iter": niter}])]
+            )
+            lb_df = pd.concat(
+                [lb_df, pd.DataFrame([{"lb": prob.value, "iter": niter}])]
+            )
             scale_new = np.linalg.lstsq(
-                G_inv @ opt_s, (y - b_bin.value).squeeze(), rcond=None
+                RG @ opt_s, (y - b_bin.value).squeeze(), rcond=None
             )[0].item()
             # est = G_inv @ opt_s + b_bin.value
             # idx = np.argmax(est)
             # scale_new = (y[idx] / est[idx]).item()
             if np.abs(scale_new - scale) <= tol:
                 break
+            elif opt_obj_last - opt_obj >= 0 and opt_obj_last - opt_obj <= tol:
+                break
             else:
                 scale = scale_new
                 niter += 1
         else:
             warnings.warn("max scale iteration reached")
-        scale_df = pd.concat(scale_df, ignore_index=True)
-        s_bin_df = pd.concat(
-            s_bin_df
-            + [
-                pd.DataFrame(
-                    {"s_bin": s.value.squeeze(), "frame": np.arange(T), "iter": -1}
-                )
-            ],
-            ignore_index=True,
-        )
-        opt_s_df = pd.concat(opt_s_df, ignore_index=True)
-        obj_df = pd.concat(obj_df, ignore_index=True)
-        lb_df = pd.concat(lb_df, ignore_index=True)
         res["C-bin"].append(G_inv @ opt_s)
         res["S-bin"].append(opt_s)
         res["b-bin"].append(b_bin.value)
@@ -233,7 +257,11 @@ for up_type, cur_YrA in {"org": YrA, "upsamp": YrA_interp}.items():
                     np.concatenate(dat, axis=1),
                     dims=["frame", "unit_id"],
                     coords={
-                        "frame": cur_YrA.coords["frame"],
+                        "frame": (
+                            C_gt_true.coords["frame"]
+                            if up_type == "upsamp"
+                            else YrA.coords["frame"]
+                        ),
                         "unit_id": A.coords["unit_id"],
                     },
                     name="-".join([vname, up_type]),
@@ -327,28 +355,18 @@ def compute_metrics(S, S_true, mets, nthres: int = None):
     return pd.concat(res_ls)
 
 
-def thresS(S, nthres):
-    Smax = S.max()
-    S_ls = [
-        (S > Smax * th).rename(S.name + "-th_{:.1f}".format(th))
-        for th in np.linspace(0.1, 0.9, nthres)
-    ]
-    return S_ls
-
-
 updt_ds = xr.open_dataset(os.path.join(INT_PATH, "temp_res.nc"))
 true_ds = open_minian(os.path.join(INT_PATH, "simulated")).isel(
     unit_id=updt_ds.coords["unit_id"]
 )
 subset = updt_ds.coords["unit_id"]
 S_gt, S_gt_true = true_ds["S"].dropna("frame", how="all"), true_ds["S_true"]
-S_org, S_bin_org, S_up, S_bin_up, YrA, YrA_up = (
+S_org, S_bin_org, S_up, S_bin_up, YrA = (
     updt_ds["S-org"].dropna("frame", how="all"),
     updt_ds["S-bin-org"].dropna("frame", how="all"),
     updt_ds["S-upsamp"],
     updt_ds["S-bin-upsamp"],
     updt_ds["YrA"].dropna("frame", how="all"),
-    updt_ds["YrA_interp"],
 )
 S_updn, S_bin_updn = (
     S_up.coarsen({"frame": 10}).sum().rename("S-updn"),
@@ -372,6 +390,7 @@ met_ds = [
 met_res = pd.concat(
     [compute_metrics(m[0], m[1], **m[2]) for m in met_ds], ignore_index=True
 )
+sns.set_theme(style="darkgrid")
 g = sns.FacetGrid(
     met_res,
     row="metric",
@@ -383,7 +402,13 @@ g = sns.FacetGrid(
     margin_titles=True,
 )
 g.map_dataframe(
-    sns.violinplot, x="variable", y="dist", bw_adjust=0.5, cut=0.3, saturation=0.6
+    sns.violinplot,
+    x="variable",
+    y="dist",
+    bw_adjust=0.5,
+    cut=0.3,
+    saturation=0.6,
+    log_scale=True,
 )
 # g.map_dataframe(sns.swarmplot, x="variable", y="dist", edgecolor="auto", linewidth=1)
 g.tick_params(axis="x", rotation=90)
